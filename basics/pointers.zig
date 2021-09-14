44204885292448f1951c6b00acfe5d0bcd25e86e
Christopher Coverdale <chris.coverdale24@gmail.com>
2021-09-14 01:06:50 +0100

    add optional pointers to basics

--- basics/pointers.zig
+++ basics/pointers.zig
@@ -39,3 +39,19 @@ test "usize" {
     try expect(@sizeOf(usize) == @sizeOf(*u8));
     try expect(@sizeOf(isize) == @sizeOf(*u8));
 }
+
+// Optional pointers allow you to use null, useful with linkedlists.
+test "optional pointers" {
+    // Pointers cannot be null. If you want a null pointer, use the optional
+    // prefix `?` to make the pointer type optional.
+    var ptr: ?*i32 = null;
+
+    var x: i32 = 1;
+    ptr = &x;
+
+    try expect(ptr.?.* == 1);
+
+    // Optional pointers are the same size as normal pointers, because pointer
+    // value 0 is used as the null value.
+    try expect(@sizeOf(?*i32) == @sizeOf(*i32));
+}
